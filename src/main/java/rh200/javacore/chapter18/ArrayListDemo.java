439c59e0011da11f17edca7f6ee4ec7426d48b9b
Rustam <rustam_hacizade@list.ru>
2022-02-21 19:32:19 +0400

    ArrayListDemo

---
+++ src/main/java/rh200/javacore/chapter18/ArrayListDemo.java
@@ -0,0 +1,32 @@
+package rh200.javacore.chapter18;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+
+        System.out.println("Начальный размер списочного массива al: " + al.size());
+
+        // ввести элементы в списочный массив
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1,"A2");
+
+        System.out.println("Размер списочного массива al после ввода элементов" + al.size());
+
+        // Вывести списочный массив
+        System.out.println("Содержимое списочного массива a1 : " + al);
+
+        // Удалить элементы из списочного массива
+        al.remove("F");
+        al.remove(2);
+
+        System.out.println("Размер списочного массива al после удаления элементов " + al.size());
+        System.out.println("Содержимое списочного массива al: " + al);
+    }
+}
