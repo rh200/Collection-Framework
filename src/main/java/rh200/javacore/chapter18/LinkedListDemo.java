4822437a69bf819ad27ead2a9572f4fd3922a618
Rustam <rustam_hacizade@list.ru>
2022-02-21 22:59:48 +0400

    LiskedListDemo

---
+++ src/main/java/rh200/javacore/chapter18/LinkedListDemo.java
@@ -0,0 +1,44 @@
+package rh200.javacore.chapter18;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        // создать связный список
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // ввести элементы в связный список
+
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+        ll.addLast("Z");
+        ll.addFirst("A");
+        ll.add(1,"A2");
+
+        System.out.println("Исходное содержимое связного списка ll: " + ll);
+
+        //удалить элементы из связного списка
+        ll.remove("F");
+        ll.remove(2);
+        System.out.println("Содержимое связного списка ll " + "после удаление элементов " + ll );
+
+        //удалить первый и последний элементы из связного списка
+        ll.removeFirst();
+        ll.removeLast();
+
+        System.out.println("Содержимое связного списка ll после удаления " + "первого и последнего элементов : " + ll);
+
+
+        //получить и присвоить значение
+        String val = ll.get(2);
+        ll.set(2,val + "изменено");
+
+        System.out.println("Содержимое связного списка 11 после изменения : " + ll);
+
+    }
+}
